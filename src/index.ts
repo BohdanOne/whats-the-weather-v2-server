a70c57dc5f149424018fd1dd001babd898506909
Bohdan <bohdanimiela@gmail.com>
2020-02-21 11:08:00 +0100

    Add cors package to project dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 import express from 'express';
+import cors from 'cors';
 import { json } from 'body-parser';
 
 import errorHandler from './middlewares/errorHandler';
@@ -9,11 +10,12 @@ import locations from './routes/locations';
 
 const app = express();
 
+app.use(cors());
 app.use(json());
 
 app.use('/locations', locations);
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`WTW server listening at port ${PORT}...`));
